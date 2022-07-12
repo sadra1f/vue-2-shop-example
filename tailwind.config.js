02c9e779ef3057d956d1292da3af732cb5a81ac0
Sadra Saderi <sadrassx@gmail.com>
2022-07-12 22:12:07 +0430

    Add initial vue files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */ /* eslint-disable */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
