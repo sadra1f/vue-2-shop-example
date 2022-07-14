62705b3bd3cecb8a28f395888b832119e769d51c
Sadra Saderi <sadrassx@gmail.com>
2022-07-14 16:08:23 +0430

    Add cart icon

--- src/components/NavbarMain.vue
+++ src/components/NavbarMain.vue
@@ -1,23 +1,31 @@
 <template>
   <nav class="w-full px-8 py-4 bg-white border-b">
     <div class="container flex items-center mx-auto">
-      <div class="title">
+      <div>
         <router-link :to="{ name: 'home' }" class="nav-title" exact>
           <h1 class="font-bold text-xl font-plus-jakarta-sans">Vue Shop</h1>
         </router-link>
       </div>
-      <div class="links">
+      <div class="nav-links">
         <router-link :to="{ name: 'home' }" class="nav-link" exact>
           Home
         </router-link>
         <router-link :to="'#'" class="nav-link" exact> Products </router-link>
+      </div>
+      <div class="nav-icons">
+        <router-link :to="'#'" class="nav-icon" exact>
+          <CartIcon />
+        </router-link>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
+import CartIcon from "./icons/CartIcon.vue";
+
 export default {
   name: "NavbarMain",
+  components: { CartIcon },
 };
 </script>
